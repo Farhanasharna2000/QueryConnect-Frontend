9dbb1e3ceeff39b3bd037a355cf3f1df57cb934a
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-06-18 12:50:44 +0600

    Complete Navbar design

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -4,7 +4,7 @@ import Footer from "../Components/Footer/Footer";
 
 const MainLayout = () => {
   return (
-    <div>     
+    <div className="text-[#1F2937]">     
       <Navbar />
       <div className="">
         <Outlet />
